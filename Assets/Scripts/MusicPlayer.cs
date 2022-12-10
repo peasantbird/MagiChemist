27a13971868b47a94785adf7109071d082c18197
Nicholas Foo <85893434+nickieleaks@users.noreply.github.com>
2022-12-10 14:42:43 +0800

    Music Shuffler

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource BGM;
+    public AudioClip[] musicArray;
+
+    AudioClip RandomTrack()
+    {
+        return musicArray[Random.Range(0, musicArray.Length)];
+    }
+    void Start()
+    {
+        BGM.PlayOneShot(RandomTrack());
+    }
+    void Update()
+    {
+        if (BGM.isPlaying == false) 
+        {
+            BGM.PlayOneShot(RandomTrack());
+        }
+    }
+}
